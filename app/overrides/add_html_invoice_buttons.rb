c222acbd9ffdeec74feab433755c850e304e7f8e
Dominik <dominik@platogo.com>
2012-11-08 10:45:43 +0100

    Update deface overrides

--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -4,8 +4,14 @@ Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :partial => "spree/admin/orders/html_buttons",
                      :disabled => false)
 
-Deface::Override.new(:virtual_path => "spree/orders/_form",
+Deface::Override.new(:virtual_path => "spree/orders/show",
                      :name => "add_html_invoice_buttons",
-                     :insert_after => "<table id='cart-detail' data-hook>",
+                     :insert_bottom => "data-hook='links'",
+                     :partial => "spree/orders/html_buttons",
+                     :disabled => false)
+
+Deface::Override.new(:virtual_path => "spree/orders/new",
+                     :name => "add_html_invoice_buttons",
+                     :insert_after => "<p><%= link_to t(:continue_shopping), products_path %></p>",
                      :partial => "spree/orders/html_buttons",
                      :disabled => false)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Spree::Core::Engine.routes.append do
-  match 'admin/invoice/:id(/:template)' => 'invoice#lasku'
+  match 'admin/orders/:id(/:template)' => 'invoice#lasku'
   match 'orders/:id(/:template)' => 'invoice#lasku'
 end
