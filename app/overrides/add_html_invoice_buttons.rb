f921305eed79c9a72db0ed8c19986185ebdc9154
Dominik <dominik@platogo.com>
2012-11-08 10:32:39 +0100

    moved vies from admin folder

--- app/overrides/add_html_invoice_buttons.rb
+++ app/overrides/add_html_invoice_buttons.rb
@@ -1,11 +1,11 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_order_tabs",
                      :name => "add_html_admin_invoice_buttons",
                      :insert_after => ".sidebar",
-                     :partial => "spree/admin/orders/html_admin_buttons",
+                     :partial => "spree/admin/orders/html_buttons",
                      :disabled => false)
 
 Deface::Override.new(:virtual_path => "spree/orders/_form",
                      :name => "add_html_invoice_buttons",
                      :insert_after => "<table id='cart-detail' data-hook>",
-                     :partial => "spree/admin/orders/html_buttons",
+                     :partial => "spree/orders/html_buttons",
                      :disabled => false)
--- lib/spree/html_invoice_configuration.rb
+++ lib/spree/html_invoice_configuration.rb
@@ -3,6 +3,7 @@ module Spree
     preference :html_invoice_logo_path, :string
     preference :html_receipt_logo_path, :string
     preference :print_buttons, :string
+    preference :print_admin_buttons , :string
     preference :suppress_anonymous_address, :boolean, :default => true
   end
 end
